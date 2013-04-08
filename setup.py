af2aa9ed92b51d143ea43b3542f410cfa07ed883
Craig Campbell <iamcraigcampbell@gmail.com>
2013-04-07 21:54:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
     long_description='Chrome Logger allows you to send server side logs to the Google Chrome console.',
     author='Craig Campbell',
     author_email='iamcraigcampbell@gmail.com',
+    url='https://github.com/ccampbell/chromelogger-python',
+    download_url='https://github.com/ccampbell/chromelogger-python/zipball/0.2.1',
+    license='Apache 2.0',
     install_requires=['jsonpickle'],
     py_modules=['chromelogger']
 )
