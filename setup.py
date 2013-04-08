7f3bc0ecdfa878ea292705a3ba8642234d009582
Craig Campbell <iamcraigcampbell@gmail.com>
2013-04-07 21:30:58 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='chromelogger',
     version='0.2.1',
     description='Python library for Chrome Logger extension.',
-    long_description='Chrome Logger allows you to send server side data to the Google Chrome console by setting X-ChromeLogger-Data header',
+    long_description='Chrome Logger allows you to send server side logs to the Google Chrome console.',
     author='Craig Campbell',
     author_email='iamcraigcampbell@gmail.com',
     install_requires=['jsonpickle'],
