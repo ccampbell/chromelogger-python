6b0759a55dfd9189e8e9b5507c8eed740220b87a
Craig Campbell <iamcraigcampbell@gmail.com>
2014-08-30 15:48:17 -0400

    Update version to 0.4.1

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 # magic
 setup(
     name='chromelogger',
-    version='0.4.0',
+    version='0.4.1',
     description='Python library for logging data to Google Chrome console.',
     long_description='Chrome Logger allows you to send server side logs from your application to the Chrome Logger extension.',
     author='Craig Campbell',
     author_email='iamcraigcampbell@gmail.com',
     url='https://github.com/ccampbell/chromelogger-python',
-    download_url='https://github.com/ccampbell/chromelogger-python/archive/0.4.0.zip#egg=chromelogger-0.4.0',
+    download_url='https://github.com/ccampbell/chromelogger-python/archive/0.4.1.zip#egg=chromelogger',
     license='Apache 2.0',
     install_requires=['jsonpickle'],
     py_modules=['chromelogger'],
